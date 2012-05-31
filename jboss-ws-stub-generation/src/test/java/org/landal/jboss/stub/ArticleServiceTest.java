255994990372c4138da8a34d91f63b47938257e4
alex <alex.landini@gmail.com>
2012-05-31 11:31:41 +0200

    little changes

--- jboss-ws-stub-generation/src/test/java/org/landal/jboss/stub/ArticleServiceTest.java
+++ jboss-ws-stub-generation/src/test/java/org/landal/jboss/stub/ArticleServiceTest.java
@@ -3,27 +3,39 @@ package org.landal.jboss.stub;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.junit.Before;
 import org.junit.Test;
 
 public class ArticleServiceTest {
-	
-	private static final Log logger = LogFactory.getLog(ArticleServiceTest.class);  
 
-	private ArticleServicePT articleServicePTPort;
+	private static final Log logger = LogFactory
+			.getLog(ArticleServiceTest.class);
 
-	@Before
-	public void setUp() {
+	@Test
+	public void testGet_WithDefaultURL() {
+
 		ArticleService articleService = new ArticleService();
-		articleServicePTPort = articleService.getArticleServicePTPort();
+		ArticleServicePT articleServicePTPort = articleService
+				.getArticleServicePTPort();
+
+		List<ArticleType> articleTypes = articleServicePTPort.getAll();
+		assertNotNull(articleTypes);
+		assertFalse(articleTypes.isEmpty());
+
+		logger.info("# of articles " + articleTypes.size());
+
 	}
 
 	@Test
-	public void testGet() {
+	public void testGet_WithCustomURL() throws MalformedURLException {
+		
+		ArticleService articleService = new ArticleService(new URL("http://www.predic8.com:8080/material/ArticleService?wsdl"));
+		ArticleServicePT articleServicePTPort = articleService.getArticleServicePTPort();
 
 		List<ArticleType> articleTypes = articleServicePTPort.getAll();
 		assertNotNull(articleTypes);
